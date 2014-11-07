979093f5321aec87b86576822f3a33765a80600d
prateekchandan <prateekchandan5545@gmail.com>
2014-11-07 17:52:18 +0530

    made 1st video

--- project/frames/make_video.sh
+++ project/frames/make_video.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Converting ppm images to jpg images....."
 mogrify -flip -format jpg -quality 100 frame_*.ppm
+echo "Succussfully Converted.."
 rm *.ppm
-ffmpeg -framerate 60 -i frame_%04d.jpg -codec copy input.mkv
+echo "cleaned.."
+echo "making video"
+ffmpeg -framerate 60 -i frame_%04d.jpg -codec copy video.mkv
+echo "made video on video.mkv"
+rm *.jpg
+echo "done"
